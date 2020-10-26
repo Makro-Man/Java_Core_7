45dd59fee64a16e64ecad774996bffe2a4533308
Makro-Man <55201116+Makro-Man@users.noreply.github.com>
2020-10-26 20:39:15 +0200

    Update AircraftControl.java

--- src/ua/lviv/lgs/lesson07/AircraftControl.java
+++ src/ua/lviv/lgs/lesson07/AircraftControl.java
@@ -1,17 +1,20 @@
 package ua.lviv.lgs.lesson07;
 
+import java.util.Random;
+
 public class AircraftControl {
+    Random r = new Random();
     void MoveUp() {
-        System.out.println("Moving Up on: "+Math.random()+" kilimeters");
+        System.out.println("Moving Up on: "+r.nextInt(100)+" kilimeters");
     }
     void MoveDown() {
-        System.out.println("Moving Down on: "+Math.random()+" kilimeters");
+        System.out.println("Moving Down on: "+r.nextInt(100)+" kilimeters");
     }
     void MoveLeft() {
-        System.out.println("Moving Left on: "+Math.random()+" kilimeters");
+        System.out.println("Moving Left on: "+r.nextInt(100)+" kilimeters");
     }
     void MoveRight() {
-        System.out.println("Moving Right on: "+Math.random()+" kilimeters");
+        System.out.println("Moving Right on: "+r.nextInt(100)+" kilimeters");
     }
 
 }
